eef6a9ad4d0f927646bc104885f6d77c45838ad6
yuneco <yukiyuneco@gmail.com>
2021-11-01 00:43:24 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import PaintCanvas from './components/PaintCanvas.vue'
+</script>
+
+<template>
+  <PaintCanvas />
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html, body, #app {
+  margin: 0;
+  position: relative;
+  height: 100%;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/PaintCanvas.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="PaintCanvas" ref="el"></div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import {PaintCanvas} from 'sym-paint'
+
+const el = ref<HTMLElement>()
+onMounted(() => {
+  console.log('moun')
+  const parent = el.value
+  if (!parent) return
+  const canvas = new PaintCanvas(parent, parent.offsetWidth, parent.offsetHeight)
+  canvas.penCount = 12
+  canvas.penColor = 'salmon'
+
+  // iOSのスクロール無効化
+  parent.addEventListener('touchmove', function (event) {
+    event.preventDefault()
+  })
+})
+</script>
+
+<style lang="scss" scoped>
+.PaintCanvas {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+}
+</style>
