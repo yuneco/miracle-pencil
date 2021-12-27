a543a355f1983858aef5439f27fec8a2109b4d89
yuneco <yukiyuneco@gmail.com>
2021-12-28 01:44:30 +0900

    [WIP]PWA設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({})],
   base: './',
   build: {
     outDir: 'build'
